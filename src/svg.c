ca605222811937a9489353cebb35792767a7b21c
Michael <michaelng12203@gmail.com>
2026-01-21 12:21:27 -0800

    Add devcontainer, add old SVGTest.cpp and svg.c files, ran makefile

--- src/svg.c
+++ src/svg.c
@@ -6,6 +6,7 @@
  */
 #include "svg.h"
 #include <stdlib.h>
+#include <stdio.h>
 
 
 /**
@@ -20,6 +21,7 @@ struct SVG_CONTEXT{
 };
 
 
+
 svg_context_ptr svg_create(svg_write_fn write_fn, 
                            svg_cleanup_fn cleanup_fn, 
                            svg_user_context_ptr user, 
@@ -30,24 +32,71 @@ svg_context_ptr svg_create(svg_write_fn write_fn,
     context->cleanup_fn = cleanup_fn;
     context->user = user;
 
+     if(width <= 0 || height <= 0){
+        return NULL;
+    }
+    if(write_fn == NULL){
+        return NULL;    
+    }
+
+    /*if(user == NULL){
+        return NULL;    
+    }*/
+
+    int size = snprintf(NULL, 0,"<svg width=\"%d\" height=\"%d\" xmlns=\"http://www.w3.org/2000/svg\">",
+                        width, height);
+    char *buffer = (char *)malloc(size + 1);
+    snprintf(buffer, size, "<svg width=\"%d\" height=\"%d\" xmlns=\"http://www.w3.org/2000/svg\">",
+                        width, height);
+
+    write_fn(user, "<?xml version=\"1.0\" encoding=\"UTF-8\"?>");
+    write_fn(user, "\n");
+    write_fn(user, buffer);
+    free(buffer);
+    write_fn(user, ">\n");
 
     return context;
+
 }
 
 svg_return_t svg_destroy(svg_context_ptr context){
-    // Enter code here
+
     if(context){
+        context->write_fn(context->user, "</svg>");
         free(context);
         return SVG_OK;
     }
     return SVG_ERR_NULL;
 }
 
+
 svg_return_t svg_circle(svg_context_ptr context,
                         const svg_point_t *center,
                         svg_real_t radius,
                         const char *style){
-    // Enter code here
+
+    if(style == NULL){
+        return SVG_ERR_IO;
+    }
+    if(radius <= 0){
+        return SVG_ERR_INVALID_ARG;
+    }
+    if(center == NULL){
+        return SVG_ERR_NULL;
+    }
+    if(context == NULL){
+        return SVG_ERR_NULL;
+    }
+    int size = snprintf(NULL, 0,"<circle cx=\"%lf\" cy=\"%lf\" r=\"%lf\" style=\"%s\"/>",
+                        center->x,center->y,radius, style);
+    char *buffer = (char *)malloc(size + 1);
+    snprintf(buffer, size, "<circle cx=\"%lf\" cy=\"%lf\" r=\"%lf\" style=\"%s\"/>",
+                        center->x,center->y,radius, style);
+    svg_return_t circle = context->write_fn(context->user, buffer);
+    free(buffer);
+    context->write_fn(context->user, ">\n");
+
+    return circle;
 }
 
 
@@ -55,21 +104,71 @@ svg_return_t svg_rect(svg_context_ptr context,
                       const svg_point_t *top_left,
                       const svg_size_t *size,
                       const char* style){
-    // Enter code here
+
+    if(style == NULL){
+        return SVG_ERR_IO;
+    }
+    if(size->width <= 0||size->height <= 0){
+        return SVG_ERR_INVALID_ARG;
+    }
+    if(top_left == NULL){
+        return SVG_ERR_NULL;
+    }   
+    if(context == NULL){
+        return SVG_ERR_NULL;
+    }
+    int space = snprintf(NULL, 0, "<rect x=\"%lf\" y=\"%lf\" width=\"%lf\" height=\"%lf\" style=\"%s\"/>",
+                        top_left->x, top_left->y, size->width, size->height, style);
+    char *buffer = (char *)malloc(space + 1);
+    snprintf(buffer, space,"<rect x=\"%lf\" y=\"%lf\" width=\"%lf\" height=\"%lf\" style=\"%s\"/>",
+                        top_left->x, top_left->y, size->width, size->height, style);
+    svg_return_t rect = context->write_fn(context->user, buffer);
+    free(buffer);
+    context->write_fn(context->user, ">\n");
+
+    return rect;
 }
 
 svg_return_t svg_line(svg_context_ptr context,
                       const svg_point_t *start,
                       const svg_point_t *end,
                       const char* style){
-    // Enter code here
+    if(style == NULL){
+        return SVG_ERR_IO;
+    }
+    if(start == NULL || end == NULL){
+        return SVG_ERR_NULL;
+    }
+    if(context == NULL){
+        return SVG_ERR_NULL;
+    }
+    int size = snprintf(NULL, 0, "<line x1=\"%lf\" y1=\"%lf\" x2=\"%lf\" y2=\"%lf\" style=\"%s\"/>",
+                        start->x, start->y, end->x, end->y, style);
+    char *buffer = (char *)malloc(size + 1);
+    snprintf(buffer, size, "<line x1=\"%lf\" y1=\"%lf\" x2=\"%lf\" y2=\"%lf\" style=\"%s\"/>",
+                        start->x, start->y, end->x, end->y, style);
+    svg_return_t line = context->write_fn(context->user, buffer);
+    free(buffer);
+    context->write_fn(context->user, ">\n");
+
+    return line;
 }
+
 
 svg_return_t svg_group_begin(svg_context_ptr context, 
                              const char* attrs){
-    // Enter code here
+    if(attrs == NULL){
+        return SVG_ERR_IO;
+    }
+    int size = snprintf(NULL, 0, "<g %s>", attrs);
+    char *buffer = (char *)malloc(size + 1);
+    snprintf(buffer, size, "<g %s>", attrs);
+    svg_return_t group_begin = context->write_fn(context->user, buffer);
+    free(buffer);
+    return group_begin;
 }
 
 svg_return_t svg_group_end(svg_context_ptr context){
-    // Enter code here
+    
+    return context->write_fn(context->user, "</g>");
 }
--- testsrc/SVGTest.cpp
+++ testsrc/SVGTest.cpp
@@ -62,49 +62,135 @@ class SVGTest : public ::testing::Test{
 
 // --- BASIC CREATION TEST ---
 TEST_F(SVGTest, CreateAndDestroy){
-
+    svg_context_ptr context = svg_create(write_callback,
+                                         cleanup_callback,
+                                         &DOutput,
+                                         100,
+                                         100);
+    EXPECT_NE(context,nullptr);
+    EXPECT_EQ(svg_destroy(context),SVG_OK);
 }
 
 // --- INVALID INPUT TESTS ---
 TEST_F(SVGTest, NullContextFunctions){
 
+   svg_point_t center = {50,50};
+    svg_point_t top_left = {50,50};
+    svg_size_t size = {30,40};
+    const svg_point_t start = {10,10};
+    svg_point_t end = {90, 90};
+
+    EXPECT_EQ(svg_circle(NULL, &center, 45, "fill:none; stroke:green; stroke-width:2"), SVG_ERR_NULL);
+    EXPECT_EQ(svg_rect(NULL, &top_left, &size, "fill:none; stroke:green; stroke-width:2"), SVG_ERR_NULL);
+    EXPECT_EQ(svg_line(NULL, &start, &end, "stroke:green; stroke-width:2"), SVG_ERR_NULL);
 }
+
+
 
 // --- DRAWING TESTS ---
 TEST_F(SVGTest, Circle){
 
+    svg_point_t center = {50,50};
+    svg_circle(DContext, &center, 45,"fill:none; stroke:green; stroke-width:2");
+    
+    std::string Allout = DOutput.JoinOutput();
+    EXPECT_NE(Allout.find("<circle"), std::string::npos);
+    EXPECT_NE(Allout.find("cx=\"50.000000\""), std::string::npos);
+    EXPECT_NE(Allout.find("cy=\"50.000000\""), std::string::npos);
+    EXPECT_NE(Allout.find("r=\"45.000000\""), std::string::npos);
+    EXPECT_NE(Allout.find("style=\"fill:none; stroke:green; stroke-width:2\""), std::string::npos);
 }
 
 TEST_F(SVGTest, Rectangle){
+    
+    svg_point_t top_left = {50,50};
+    svg_size_t size = {30,40};
+    svg_rect(DContext, &top_left, &size,"fill:none; stroke:green; stroke-width:2");
 
+
+    std::string Allout = DOutput.JoinOutput();
+    EXPECT_NE(Allout.find("<rect"), std::string::npos);
+    EXPECT_NE(Allout.find("x=\"50.000000\""), std::string::npos);
+    EXPECT_NE(Allout.find("y=\"50.000000\""), std::string::npos);
+    EXPECT_NE(Allout.find("width=\"30.000000\""), std::string::npos);
+    EXPECT_NE(Allout.find("height=\"40.000000\""), std::string::npos);
+    EXPECT_NE(Allout.find("style=\"fill:none; stroke:green; stroke-width:2\""), std::string::npos);
 }
 
 TEST_F(SVGTest, Line){
+    
+    const svg_point_t start = {10,10};
+    svg_point_t end = {90, 90};
+    svg_line(DContext, &start, &end, "stroke:green; stroke-width:2");
 
+    std::string Allout = DOutput.JoinOutput();
+    EXPECT_NE(Allout.find("<line"), std::string::npos);
+    EXPECT_NE(Allout.find("x1=\"10.000000\""), std::string::npos);
+    EXPECT_NE(Allout.find("y1=\"10.000000\""), std::string::npos);
+    EXPECT_NE(Allout.find("x2=\"90.000000\""), std::string::npos);
+    EXPECT_NE(Allout.find("y2=\"90.000000\""), std::string::npos);
+    EXPECT_NE(Allout.find("style=\"stroke:green; stroke-width:2\""), std::string::npos);
 }
 
 // --- GROUPING TEST ---
 TEST_F(SVGTest, Grouping){
+    
+    svg_group_begin(DContext, "stroke:blue; stroke-width:2>");
+    svg_group_end(DContext);
+
+    std::string Allout = DOutput.JoinOutput();
+    EXPECT_NE(Allout.find("<g"), std::string::npos);
+    EXPECT_NE(Allout.find("stroke:blue; stroke-width:2>"), std::string::npos);
+    EXPECT_NE(Allout.find("</g>"), std::string::npos);
 
 }
+
+
+
+
 
 // --- EDGE CASES ---
 TEST_F(SVGTest, ZeroDimensions){
+    
+    svg_point_t center = {50,50};
+    svg_point_t top_left = {50,50};
+    svg_size_t size = {0,0};
+
+    EXPECT_EQ(svg_circle(DContext, &center, 0, "fill:none; stroke:green; stroke-width:2"), SVG_ERR_INVALID_ARG);
+    EXPECT_EQ(svg_rect(DContext, &top_left, &size, "fill:none; stroke:green; stroke-width:2"), SVG_ERR_INVALID_ARG);
+
 
 }
+
+
 
 TEST_F(SVGTest, NullPointPointer){
+    
+    svg_size_t size = {30,40};
 
+    EXPECT_EQ(svg_circle(DContext, nullptr, 45, "fill:none; stroke:green; stroke-width:2"), SVG_ERR_NULL);
+    EXPECT_EQ(svg_rect(DContext, nullptr, &size, "fill:none; stroke:green; stroke-width:2"), SVG_ERR_NULL);
+    EXPECT_EQ(svg_line(DContext, nullptr, nullptr, "fill:none; stroke:green; stroke-width:2"), SVG_ERR_NULL);
 }
+
+
 
 TEST_F(SVGTest, CreateEdgeCases){
-
-}
+    
+    EXPECT_EQ(svg_create(write_callback, cleanup_callback, &DOutput, 0, 0), nullptr);
+    EXPECT_EQ(svg_create(nullptr, cleanup_callback, &DOutput, 100, 100), nullptr);
+}   
 
 TEST_F(SVGTest, DestroyEdgeCases){
+    
+svg_context_ptr context = svg_create(write_callback,cleanup_callback,&DOutput,100,100);    
+svg_return_t destroy = svg_destroy(context);
+
+    std::string Allout = DOutput.JoinOutput();
+    EXPECT_NE(Allout.find("</svg>"), std::string::npos);
+    EXPECT_EQ(svg_destroy(NULL), SVG_ERR_NULL);
 
 }
-
 
 // Callback to capture SVG output
 svg_return_t write_error_callback(svg_user_context_ptr user, const char* text){
@@ -116,6 +202,17 @@ svg_return_t write_error_callback(svg_user_context_ptr user, const char* text){
     return SVG_ERR_IO;
 }
 
+// --- IO Errors ---
 TEST_F(SVGTest, IOErrorTest){
 
+    svg_point_t center = {50,50};
+    svg_point_t top_left = {50,50};
+    svg_size_t size = {30,40};
+    const svg_point_t start = {10,10};
+    svg_point_t end = {90, 90};
+
+    EXPECT_EQ(svg_circle(DContext, &center, 45, NULL), SVG_ERR_IO);
+    EXPECT_EQ(svg_rect(DContext, &top_left, &size, NULL), SVG_ERR_IO);
+    EXPECT_EQ(svg_line(DContext, &start, &end, NULL), SVG_ERR_IO);
+    EXPECT_EQ(svg_group_begin(DContext, NULL), SVG_ERR_IO);
 }
